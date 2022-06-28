3631215f933c21ce1d5f9ee43b3f4947d590457c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-28 21:04:09 +0300

    Change can_be_archived to private in the parent class

--- movie.rb
+++ movie.rb
@@ -3,10 +3,10 @@ require_relative './item'
 class Movie < Item
   attr_accessor :silet
 
-  def initialize(silet) {
-    super
+  def initialize publish_date, silet
+    super(publish_date)
     @silet = silet
-  }
+  end
 
   def can_be_archived?
     super or @silet
