f892f001779016c0b9991738712fa3a2ccfb04c5
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-28 20:49:12 +0300

    Override private instance method can_be_archived

--- movie.rb
+++ movie.rb
@@ -1,2 +1,15 @@
-class Movie
+require_relative './item'
+
+class Movie < Item
+  attr_accessor :silet
+
+  def initialize(silet) {
+    super
+    @silet = silet
+  }
+
+  def can_be_archived?
+    super or @silet
+  end
+
 end
