83ee12c99b988875dc658b41643e162d5b49e556
Achilles-Dev <solohag92@gmail.com>
2022-06-28 17:43:45 +0000

    add method to game class

--- game.rb
+++ game.rb
@@ -1,6 +1,14 @@
-class Game
-  def initialize (multiplayer, last_played_at)
+require './item'
+
+class Game < Item
+  def initialize(multiplayer, last_played_at)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  private
+
+  def can_be_archived?()
+    true if super == true || last_played_at > 2
   end
 end
