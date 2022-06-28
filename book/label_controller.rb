7ef2756a9dac6f0f1cc857c993aa6f2a720253cd
levy002 <levy.ukwi002@gmail.com>
2022-06-28 22:05:50 +0200

    Create label controller class

---
+++ book/label_controller.rb
@@ -0,0 +1,37 @@
+require 'json'
+require_relative 'data'
+require_relative 'label' 
+
+  class LabelController
+    def initialize
+        @labels = read_labels
+        all_labels = File.read('./book/json_files/label.json')
+        File.write('./book/json_files/label.json', []) if all_labels.empty?
+    end
+
+  def add_label
+    puts 'Please fill the following information:'
+    puts ''
+    puts 'Title:'
+    title = gets.chomp
+
+    puts 'Color:'
+    color = gets.chomp
+    new_label = Label.new(title, color)
+    write_labels(new_label)
+    puts 'Label creted successfully'
+  end
+
+  def labels_list
+      if @labels.empty?
+        puts 'No labels available!'
+      else
+      puts
+      puts 'The labels list: '
+      puts
+      @labels.each_with_index do |label, index|
+        puts "#{index + 1}) label details: Title: #{label.title} | Color: #{label.color} | Items: #{label.items}"
+        end
+    end
+  end
+end  
