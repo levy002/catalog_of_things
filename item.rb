77ff2ea6a45c1295401e92613fa4fd17909d47f0
levy002 <levy.ukwi002@gmail.com>
2022-06-28 12:42:14 +0200

    Fix rubocop

--- item.rb
+++ item.rb
@@ -1,27 +1,25 @@
 class Item
+  attr_reader :archived, :label, :author, :genre
+  attr_accessor :publish_date
 
-    attr_reader :archived, :label, :author, :genre
-    attr_accessor :publish_date
+  def initialize(publish_date)
+    @id = Random.rand(1...1000)
+    @archived = false
+    @publish_date = publish_date
+  end
 
-    def initialize(publish_date: publish_date)
-        @id = Random.rand(1...1000)
-        @archived = false
-        @publish_date = publish_date
-        
-    end
-    
-    def can_be_archived?
-        diff = (Time.now.year) - @publish_date
-        
-        diff > 10
-    end
+  def can_be_archived?
+    diff = Time.now.year - @publish_date
 
-    def move_to_archive
-        @archived = true if can_be_archived?
-    end
+    diff > 10
+  end
 
-    def add_label(label)
-        @label = label
-        @label.add_item()
-    end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def add_label(label)
+    @label = label
+    @label.add_item
+  end
 end
