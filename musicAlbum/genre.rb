ad2152837c7a4a82728dc15fe1c3a025e030b94e
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-06-28 18:10:37 +0300

    Update checked and fixed all found offences

--- musicAlbum/genre.rb
+++ musicAlbum/genre.rb
@@ -1,14 +1,14 @@
-class Genre 
-    attr_accessor :items
+class Genre
+  attr_accessor :items
 
-    def initialize(name)
-        @id = Random.rand(1...1000)
-        @name = name
-        @items = []
-    end
+  def initialize(name)
+    @id = Random.rand(1...1000)
+    @name = name
+    @items = []
+  end
 
-    def add_item(item)
-        @items.push(item)
-        item.genre = self 
-    end
+  def add_item(item)
+    @items.push(item)
+    item.genre = self
+  end
 end
--- musicAlbum/musicalbum.rb
+++ musicAlbum/musicalbum.rb
@@ -1,13 +1,12 @@
 require_relative '../item'
 
-class MusicAlbum 
-    def initialize(on_spotify)
-        super()
-        @on_spotify = false
-    end
+class MusicAlbum
+  def initialize(_on_spotify)
+    super()
+    @on_spotify = false
+  end
 
-    def can_be_archived?()
-        @archived = true if Item.can_be_archived? && @on_spotify
-    end
+  def can_be_archived?()
+    @archived = true if Item.can_be_archived? && @on_spotify
+  end
 end
-
