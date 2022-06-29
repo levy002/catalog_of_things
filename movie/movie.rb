1685aa288065740b0a667b5e501697d12b35ffc3
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-29 09:12:07 +0300

    Fix linter errors using rubocop

--- movie/movie.rb
+++ movie/movie.rb
@@ -3,7 +3,7 @@ require_relative '../item'
 class Movie < Item
   attr_accessor :silet
 
-  def initialize publish_date, silet
+  def initialize(publish_date, silet)
     super(publish_date)
     @silet = silet
   end
@@ -11,5 +11,4 @@ class Movie < Item
   def can_be_archived?
     super or @silet
   end
-
 end
