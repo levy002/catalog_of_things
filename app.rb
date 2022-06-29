7ae145c9b49f4cd7266a44650596db650db4d5d8
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-06-29 16:01:30 +0300

    Update check and resolve merge conflicts

--- app.rb
+++ app.rb
@@ -1,13 +1,39 @@
 require_relative './musicAlbum/music_controller'
+require './book/book_controller'
+require './book/label_controller'
 
 class App
   attr_accessor :music_controller
+  attr_reader :label_controller
 
   def initialize
+    @book_controller = BookController.new
+    @label_controller = LabelController.new
     @music_controller = MusicController.new
   end
 
-  def books; end
+  def books
+    puts "
+       1. List all books
+       2. List all labels
+       3. Add new book
+       4. Add new label
+    "
+    choice = gets.chomp
+    case choice
+    when '1'
+      @book_controller.books_list
+    when '2'
+      @label_controller.labels_list
+    when '3'
+      @book_controller.add_book
+    when '4'
+      @label_controller.add_label
+    else
+      puts 'Invalid choice'
+      books
+    end
+  end
 
   def music
     print 'Hello! What tasks would you like to perform?:
