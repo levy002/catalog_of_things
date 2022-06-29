8088f6308e951610c96045c36a398a57c3bf189b
Achilles-Dev <solohag92@gmail.com>
2022-06-29 17:29:25 +0000

    add save_author method to save_data file

--- game/read_data.rb
+++ game/read_data.rb
@@ -3,11 +3,19 @@ require 'json'
 def read_games
   file = File.read('games.json') if File.exist?('games.json')
   games = JSON.parse(file) unless file.chomp.empty?
-  @method.game_controller.games = games.map { |book| Game.new(game['multiplayer'], game['last_played_at']) } || []
+  @method.game_controller.games = games.map { |game| Game.new(game['multiplayer'], game['last_played_at']) } || []
 end
 
 def read_authors
   file = File.read('authors.json') if File.exist?('authors.json')
   authors = JSON.parse(file) unless file.chomp.empty?
-  @method.game_controller.authors = authors.map { |author| Game.new(author['multiplayer'], author['last_played_at']) } || []
+  add_authors(authors)
+end
+
+def add_authors(authors)
+  @method.game_controller.authors = authors.map do |author|
+    author_game = @method.game_controller.games.find { |game| game.id == author['item_id'] }
+    new_author = Author.new(author['first_name'], author['last_name'])
+    new_author.add_item(author_game)
+  end || []
 end
--- game/save_data.rb
+++ game/save_data.rb
@@ -5,3 +5,11 @@ def save_game
   games = JSON.generate(games_array)
   File.write('games.json', games)
 end
+
+def save_author
+  author_array = @method.game_controller.authors do |author|
+    { class_instance: 'Author', first_name: author.first_name, last_name: author.last_name, item_id: author.item.id }
+  end
+  authors = JSON.generate(author_array)
+  File.write('authors.json', authors)
+end
