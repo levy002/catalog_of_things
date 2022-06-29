d699fab96c73ab389f0de53ba07158b45bd69ffe
Achilles-Dev <solohag92@gmail.com>
2022-06-29 10:21:10 +0000

    Add method games and initialize to Game class

---
+++ Game/game_controller.rb
@@ -0,0 +1,3 @@
+class GameController
+  puts 'Game'
+end
--- app.rb
+++ app.rb
@@ -1,9 +1,34 @@
+require_relative '../game_controller'
 class App
+  attr_accessor :game_controller
+
+  def initialize
+    @game_controller = GameController.new
+  end
+
   def books; end
 
   def music; end
 
-  def games; end
+  def games
+    puts "
+      1. List all games
+      2. List all authors
+      3. Add new book
+    "
+    choice = gets.chomp
+    case choice
+    when '1'
+      @game_controller.list_games
+    when '2'
+      @game_controller.list_authors
+    when '3'
+      @game_controller.add_game
+    else
+      puts 'Invalid choice'
+      games
+    end
+  end
 
   def movies; end
 end
