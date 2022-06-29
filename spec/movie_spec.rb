fb0354162cbd0ec86e41ecad6b93e0c0bdd477f4
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-29 17:29:05 +0300

    Write unit tests for the Movie class

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -5,9 +5,16 @@ describe Movie do
     @movie = Movie.new(1990, true)
   end
   
-  context 'Create @movie, an instance of class Movie' do
+  context 'Create @movie, an instance of Movie class' do
     it 'takes two parameters and returns Movie object' do
       expect(@movie).to be_instance_of Movie
+    end
+  end
+
+  context 'The Movie class' do
+    it 'returns the correct boolean when initialized with true value' do
+      expected_value = true
+      expect(@movie.silet).to be(expected_value)
     end
   end
 
