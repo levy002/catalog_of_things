d22f4259af79b5f30f85629e6011d661db77e57d
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-06-29 17:20:06 +0300

    Update added the list all genres method

--- musicAlbum/genre_controller.rb
+++ musicAlbum/genre_controller.rb
@@ -7,6 +7,16 @@ class GenreController
     @genres = []
   end
 
+  def list_all_genres
+    if @genres.length.zero?
+      puts 'You don\'t have any genres available'
+    else
+      @genres.each_with_index do |gen, index|
+        puts "#{index}) Genre name: #{gen.name}."
+      end
+    end
+  end
+
   def add_genre
     puts 'Great! let\'s create the genre!'
     print 'What is the name of the genre? '
