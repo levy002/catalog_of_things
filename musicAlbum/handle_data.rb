8530dc3607e6f12338719f36c27415fd3866de92
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-06-29 22:13:08 +0300

    Update save music albums to json file

--- musicAlbum/handle_data.rb
+++ musicAlbum/handle_data.rb
@@ -3,12 +3,13 @@ require_relative './genre_controller'
 require_relative './music_controller'
 
 def save_musics(musics)
-    mq = []
-    musics.each do |music|
-      mq.push({
-                "on_spotify": music.on_spotify,
-                "publish_date": music.publish_date
-              })
-    end
-    File.write('./musicAlbum/data/musics.json', JSON.generate(mq).to_s)
+  mq = []
+  musics.each do |music|
+    mq.push({
+              "on_spotify": music.on_spotify,
+              "publish_date": music.publish_date
+            })
+  end
+  File.write('./musicAlbum/data/musics.json', JSON.generate(mq).to_s)
 end
+
