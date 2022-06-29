d5d4adf5c3c832664af225cac3ae8fbfa4e8ad67
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-06-29 07:24:35 +0300

    Update made requested changes

--- musicAlbum/musicalbum.rb
+++ musicAlbum/musicalbum.rb
@@ -1,12 +1,14 @@
 require_relative '../item'
 
-class MusicAlbum
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
   def initialize(_on_spotify)
     super()
     @on_spotify = false
   end
 
   def can_be_archived?()
-    @archived = true if Item.can_be_archived? && @on_spotify
+    true if super() && @on_spotify
   end
 end
