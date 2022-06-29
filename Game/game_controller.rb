908a92fd5c28fb409ec74710f056fa304aeb99af
Achilles-Dev <solohag92@gmail.com>
2022-06-29 10:25:03 +0000

    add method add_game to Game class

--- Game/game_controller.rb
+++ Game/game_controller.rb
@@ -8,4 +8,16 @@ class GameController
     @games = []
     @authors = []
   end
+
+  def add_game
+    puts 'Please fill the following information:'
+    puts ''
+    puts 'Enter your name (Multiplayer): '
+    multiplayer = $stdin.gets.chomp
+    print 'When last did you play("yyyy-mm-dd"): '
+    last_played_at = $stdin.gets.chomp
+    game = Game.new(multiplayer, last_played_at)
+    @game_controller << game unless @game_controller.include?(game)
+    puts 'Person successfully created'
+  end
 end
