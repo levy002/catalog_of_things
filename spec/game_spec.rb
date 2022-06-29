572a4a9fb8f05a05449fb60c7e342ed14e687681
Achilles-Dev <solohag92@gmail.com>
2022-06-29 12:43:42 +0000

    update tests for can_be_archived? method in Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,25 +1,26 @@
 require_relative '../game/game'
+describe do
+  describe '#can_be_archived?' do
+    before :each do
+      @game = Game.new('Solomon', '2019', 2020)
+    end
 
-describe '#can_be_archived?' do
-  before :each do
-    @game = Game.new('Solomon', '2019', 2020)
-  end
+    it 'returns true ' do
+      expected_value = false
+      expect(@game.can_be_archived?).to eq(expected_value)
+    end
 
-  it 'returns true ' do
-    expected_value = false
-    expect(@game.can_be_archived?).to eq(expected_value)
-  end
+    it 'returns false ' do
+      @game.publish_date = 2002
+      expected_value = true
+      expect(@game.can_be_archived?).to eq(expected_value)
+    end
 
-  it 'returns false ' do
-    @game.publish_date = 2002
-    expected_value = true
-    expect(@game.can_be_archived?).to eq(expected_value)
-  end
-
-  it 'returns false ' do
-    @game.publish_date = 2002
-    @game.last_played_at = '2021'
-    expected_value = false
-    expect(@game.can_be_archived?).to eq(expected_value)
+    it 'returns false ' do
+      @game.publish_date = 2002
+      @game.last_played_at = '2021'
+      expected_value = false
+      expect(@game.can_be_archived?).to eq(expected_value)
+    end
   end
 end
