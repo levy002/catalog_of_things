4e6945be53c004093a6e0af75ea2475e6d60164b
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-06-30 13:19:20 +0300

    Update created the genre table schema

--- database/schema.sql
+++ database/schema.sql
@@ -15,3 +15,9 @@ CREATE TABLE book(
   label_id  INT,
   FOREIGN KEY (label_id) REFERENCES label(id),
 );
+
+CREATE TABLE IF NOT EXISTS genre (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(80),
+    PRIMARY KEY(id)
+);
