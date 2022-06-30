248190b81b7c25a4404152424286e73654540228
Achilles-Dev <solohag92@gmail.com>
2022-06-30 03:35:38 +0000

    Update schema table for games

--- schema.sql
+++ schema.sql
@@ -3,16 +3,10 @@ CREATE TABLE games (
   multiplayer VARCHAR(100) NOT NULL,
   last_played_at DATE NOT NULL,
   publish_date DATE,
-  genre_id INT,
   author_id INT,
-  source_id INT,
-  label_id INT,
   archived BOOLEAN,
   PRIMARY KEY (id)
-  FOREIGN KEY (genre_id) REFERENCES genres (id),
   FOREIGN KEY (author_id) REFERENCES authors (id)
-  FOREIGN KEY (source_id) REFERENCES sources (id)
-  FOREIGN KEY (label_id) REFERENCES labels (id)
 );
 
 CREATE TABLE authors (
