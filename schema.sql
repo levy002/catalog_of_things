adc1f981be18574a91bd050c431d2feebc4dcfe8
Achilles-Dev <solohag92@gmail.com>
2022-06-30 03:26:53 +0000

    add schema table for author

--- schema.sql
+++ schema.sql
@@ -1,16 +1,23 @@
 CREATE TABLE games (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    multiplayer VARCHAR(100) NOT NULL,
-    last_played_at DATE NOT NULL,
-    publish_date DATE,
-    genre_id INT,
-    author_id INT,
-    source_id INT,
-    label_id INT,
-    archived BOOLEAN,
-    PRIMARY KEY (id)
-    FOREIGN KEY (genre_id) REFERENCES genre (id),
-    FOREIGN KEY (author_id) REFERENCES author (id)
-    FOREIGN KEY (source_id) REFERENCES source (id)
-    FOREIGN KEY (label_id) REFERENCES label (id)
+  id INT GENERATED ALWAYS AS IDENTITY,
+  multiplayer VARCHAR(100) NOT NULL,
+  last_played_at DATE NOT NULL,
+  publish_date DATE,
+  genre_id INT,
+  author_id INT,
+  source_id INT,
+  label_id INT,
+  archived BOOLEAN,
+  PRIMARY KEY (id)
+  FOREIGN KEY (genre_id) REFERENCES genre (id),
+  FOREIGN KEY (author_id) REFERENCES author (id)
+  FOREIGN KEY (source_id) REFERENCES source (id)
+  FOREIGN KEY (label_id) REFERENCES label (id)
+);
+
+CREATE TABLE author (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
 );
