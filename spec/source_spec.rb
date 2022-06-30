9d053e7a55e1076b188d452cf8af452aeb7c1d7d
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-30 11:23:00 +0300

    Add tests for items property of Source class

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -5,18 +5,22 @@ describe Source do
     @source = Source.new('SourceOne')
   end
   
-  context 'Create @movie, an instance of Movie class' do
-    it 'takes two parameters and returns Movie object' do
+  context 'Create @source, an instance of Source class' do
+    it 'takes two parameters and returns Source object' do
       expect(@source).to be_instance_of Source
     end
   end
 
-  context 'The Movie class' do
+  context 'The Source class' do
     it 'returns the correct name its initialized with' do
       expected_value = 'SourceOne'
       expect(@source.name).to eq(expected_value)
     end
 
+    it 'returns returns empty array for items property' do
+      expected_value = []
+      expect(@source.items).to eq(expected_value)
+    end
   end
 
 end
