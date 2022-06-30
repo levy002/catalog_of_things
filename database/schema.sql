3b84a4729d6070072c9c2927f30d1aa9bd25a656
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-06-30 13:33:04 +0300

    Update created the musicalbum table schema

--- database/schema.sql
+++ database/schema.sql
@@ -21,3 +21,13 @@ CREATE TABLE IF NOT EXISTS genre (
     name VARCHAR(80),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS musicalbum (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    on_spotify BOOLEAN,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL,
+    genre_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT genre_id_fk FOREIGN KEY(genre_id) REFERENCES genre(id),
+);
